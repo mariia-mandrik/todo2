8533c45cb49792931aa50d9df0cb140dd19f9184
Mariia Mandrik <manyanyka462@gmail.com>
2020-05-05 22:41:30 +0300

    todo app withouy css

---
+++ todojs.js
@@ -0,0 +1,25 @@
+
+class Todojs {
+    constructor(inp, list){
+        this.inp = inp;
+        this.list = list;
+    }
+    initHandler(){
+        let list =this.list
+        $(this.inp).keypress(function(event){
+            if ( event.which == 13 ) {
+                let random = Math.random();
+                $(list).append( `<div id="${random}">
+                    ${$("#inp").val()}
+                    <button id="${random}">delete</button>
+                    </div>`);
+            }
+        });
+
+        $(document).on('click','button[id^=0]', function(){
+            $(this).closest( "div[id^=0]" ).remove();
+        });
+    }
+}
+
+new Todojs("#inp","#list").initHandler();
